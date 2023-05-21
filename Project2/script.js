6db2501ac14494ee263520920742e88e9bbdb7c8
Orçun Türkokuloğlu <34009930+shinnok18@users.noreply.github.com>
2023-05-21 19:39:13 +0300

    Add files via upload

---
+++ Project2/script.js
@@ -0,0 +1,150 @@
+$( function() {
+  $( "#birthday" ).datepicker();
+} );
+
+
+$(document).ready(function() {
+  $.validator.addMethod("phoneFormat", function(value, element) {
+    return this.optional(element) || /^[\d+-]+$/.test(value);
+  }, "Please enter a valid phone number");
+  
+  $('#myForm').submit(function(e) {
+    e.preventDefault(); 
+    
+    if ($('#myForm').valid()) {
+      if (confirm("Are you sure you want to submit the form?")) {
+        this.submit(); 
+      } else {
+
+      }
+    }
+  });
+
+  $('#myForm').validate({
+    rules: {
+      message: {
+        required: true,
+      },
+      name: 'required',
+      surname: 'required',
+      email: {
+        required: true,
+        email: true
+      },
+      phone: {
+        required: true,
+        phoneFormat: true
+      },
+      birthday: 'required',
+      'message-title': 'required',
+      message2: {
+        required: true,
+        minlength: 10
+      },
+      radio: {
+        required: true
+      }
+    },
+    messages: {
+      message: 'Please select a message subject',
+      name: 'Please enter your name',
+      surname: 'Please enter your surname',
+      email: {
+        required: 'Please enter your email address',
+        email: 'Please enter a valid email address'
+      },
+      phone: {
+        required: 'Please enter your phone number',
+        phoneFormat: 'Please enter a valid phone number'
+      },
+      birthday: 'Please enter your date of birth',
+      'message-title': 'Please enter the message title',
+      message2: {
+        required: 'Please enter your message',
+        minlength: 'Please enter at least 10 characters'
+      },
+      radio: 'Please select a gender'
+    },
+    errorPlacement: function(error, element) {
+      if (element.attr('name') === 'radio') {
+        error.insertAfter($('.radio-group:last'));
+      } else if (element.attr('name') === 'message2') {
+        error.insertAfter($('#message2'));
+      } else {
+        error.insertAfter(element);
+      }
+    }
+
+  });
+
+});
+
+
+$(document).ready(function() {
+  $("input[type='radio']").checkboxradio();
+});
+
+
+
+$(document).ready(function() {
+    $('.image-link').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+$(document).ready(function() {
+    $('.image-link2').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+});
+
+$(document).ready(function() {
+    $('.name2').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+});
+
+$(document).ready(function() {
+  $('#video-player').on('click', function() {
+    var video = document.getElementById('video');
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
+
+  $(document).ready(function() {
+    var latitude = 41.01393044426409;
+    var longitude = 28.54310699572662;
+
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+      .bindPopup('U.S Polo Center Office')
+      .openPopup();
+  });
+
+
+
+
+
+
+
+
+
